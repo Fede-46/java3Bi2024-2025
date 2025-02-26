01ca3a44ca09c7b463da05cbbdca8d97191930d0
Federico-Agostini <federico.agostini@marconirovereto.it>
2025-02-26 15:25:31 +0000

    (feat) Esercizio 7

--- Vettori/Es_7.java
+++ Vettori/Es_7.java
@@ -1,1 +1,34 @@
 // 7. Determinare se la stringa è palindroma
+
+import java.util.Scanner;
+
+public class Palindroma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digita una stringa:");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        if (isPalindroma(input)) {
+            System.out.println("La stringa è palindroma.");
+        } else {
+            System.out.println("La stringa non è palindroma.");
+        }
+    }
+
+    public static boolean isPalindroma(String testo) {
+        String testoLower = testo.toLowerCase(); 
+        int inizio = 0;
+        int fine = testoLower.length() - 1;
+        
+        while (inizio < fine) {
+            if (testoLower.charAt(inizio) != testoLower.charAt(fine)) {
+                return false; 
+            }
+            inizio++;
+            fine--;
+        }
+        
+        return true; 
+    }
+}
