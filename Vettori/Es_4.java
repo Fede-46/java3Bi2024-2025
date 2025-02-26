7ba2424e2ece18661b8b10524e2ff8ddb022cdb2
Federico-Agostini <federico.agostini@marconirovereto.it>
2025-02-26 15:19:19 +0000

    (feat) Esercizio 4

--- Vettori/Es_4.java
+++ Vettori/Es_4.java
@@ -1,1 +1,20 @@
 // 4. Invertite la stringa
+
+import java.util.Scanner;
+
+public class InvertiStringa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digita una stringa da invertire:");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        String stringaInvertita = invertiStringa(input);
+        System.out.println("Stringa invertita: " + stringaInvertita);
+    }
+
+    public static String invertiStringa(String testo) {
+        StringBuilder sb = new StringBuilder(testo);
+        return sb.reverse().toString();  
+    }
+}
